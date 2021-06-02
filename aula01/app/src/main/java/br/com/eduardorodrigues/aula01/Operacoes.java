03912e90b8202a47cb45c38e521a15f6b512d630
Eduardo Rodrigues Santana <eduardo.r.santana@hotmail.com>
2021-06-02 19:52:59 -0300

    Programming in Java for Android

---
+++ aula01/app/src/main/java/br/com/eduardorodrigues/aula01/Operacoes.java
@@ -0,0 +1,28 @@
+package br.com.eduardorodrigues.aula01;
+
+class Operacoes {
+
+    private float a;
+    private float b;
+
+    public Operacoes(float a, float b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public float soma() {
+        return a + b;
+    }
+
+    public float subtracao() {
+        return a - b;
+    }
+
+    public float multiplicacao() {
+        return a * b;
+    }
+
+    public float divisao() {
+        return a / b;
+    }
+}
