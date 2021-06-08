f85e10bec2236a69acfa2df1ffad0babec8f53b1
Eduardo Rodrigues Santana <eduardo.r.santana@hotmail.com>
2021-06-08 18:05:09 -0300

    Captures the text typed by the user and displays it in a Toast when clicking the button

--- aula02/app/src/main/java/br/com/eduardorodrigues/aula02/MainActivity.java
+++ aula02/app/src/main/java/br/com/eduardorodrigues/aula02/MainActivity.java
@@ -17,6 +17,7 @@ import br.com.eduardorodrigues.aula02.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,14 +25,20 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
     private int count;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /*  A activity (tela) inicia capturando o id do elemento tipo TextView e inicializando
+            a varivael contatdor count
         textView = findViewById(R.id.textView);
-        count = 0;
+        count = 0;*/
+
+        /*  A activity (tela) inicia capturando o id do elemento tipo EditText */
+        editText = findViewById(R.id.editText);
     }
 
     public void onClick(View view) {
@@ -40,10 +47,16 @@ public class MainActivity extends AppCompatActivity {
         Toast toast = Toast.makeText(this, "Você pressionou o botão", Toast.LENGTH_SHORT);
         toast.show();*/
 
-        /*  Ao pressionao o botão aparece a mensagem: "Você pressionou o botão" acompanhado das
-            vezes que o botão foi clicado*/
+        /*  Ao pressionar o botão aparece a mensagem: "Você pressionou o botão" acompanhado das
+            vezes que o botão foi clicado
         count++;
-        textView.setText("Você pressionou o botão: " + count);
+        textView.setText("Você pressionou o botão: " + count);*/
+
+        /*  Ao pressionar o botão aparece a mensagem: "Você escreveu" acompanhado do que foi digitado
+            pelo usuário */
+        String txt = editText.getText().toString();
+        Toast toast = Toast.makeText(this, "Você escreveu: " + txt, Toast.LENGTH_SHORT);
+        toast.show();
 
     }
 }
