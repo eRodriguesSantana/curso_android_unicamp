43159da9fb92a59756941a003cf50efaf8437d35
Eduardo Rodrigues Santana <eduardo.r.santana@hotmail.com>
2021-06-08 17:48:21 -0300

    When clicking the button, the number of times it was clicked is displayed

--- aula02/app/src/main/java/br/com/eduardorodrigues/aula02/MainActivity.java
+++ aula02/app/src/main/java/br/com/eduardorodrigues/aula02/MainActivity.java
@@ -17,18 +17,33 @@ import br.com.eduardorodrigues.aula02.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        count = 0;
     }
 
     public void onClick(View view) {
+        /*
+            Ao pressionar o botão aparece a mensagem: "Você pressionou o botão"
         Toast toast = Toast.makeText(this, "Você pressionou o botão", Toast.LENGTH_SHORT);
-        toast.show();
+        toast.show();*/
+
+        /*  Ao pressionao o botão aparece a mensagem: "Você pressionou o botão" acompanhado das
+            vezes que o botão foi clicado*/
+        count++;
+        textView.setText("Você pressionou o botão: " + count);
+
     }
 }
