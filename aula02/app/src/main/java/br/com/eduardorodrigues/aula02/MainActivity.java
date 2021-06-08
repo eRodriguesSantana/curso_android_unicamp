768d444028d93bc451bd2d043a398a0568b65f3f
Eduardo Rodrigues Santana <eduardo.r.santana@hotmail.com>
2021-06-08 17:25:33 -0300

    Method insertion that returns a Toast when button is clicked

---
+++ aula02/app/src/main/java/br/com/eduardorodrigues/aula02/MainActivity.java
@@ -0,0 +1,34 @@
+package br.com.eduardorodrigues.aula02;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import br.com.eduardorodrigues.aula02.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        Toast toast = Toast.makeText(this, "Você pressionou o botão", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}
