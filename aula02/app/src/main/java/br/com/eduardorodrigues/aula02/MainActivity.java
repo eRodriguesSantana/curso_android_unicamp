09e5815c8d625827554001fbc515723f33b5d20d
Eduardo Rodrigues Santana <eduardo.r.santana@hotmail.com>
2021-06-10 11:52:06 -0300

    Captures the checkbox selected by the user and displays them in a Toast when clicking the button

--- aula02/app/src/main/java/br/com/eduardorodrigues/aula02/MainActivity.java
+++ aula02/app/src/main/java/br/com/eduardorodrigues/aula02/MainActivity.java
@@ -17,6 +17,7 @@ import br.com.eduardorodrigues.aula02.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView textView;
     private int count;
     private EditText editText;
+    private CheckBox checkBox_1;
+    private CheckBox checkBox_2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,13 +40,17 @@ public class MainActivity extends AppCompatActivity {
         textView = findViewById(R.id.textView);
         count = 0;*/
 
-        /*  A activity (tela) inicia capturando o id do elemento tipo EditText */
-        editText = findViewById(R.id.editText);
+        /*  A activity (tela) inicia capturando o id do elemento tipo EditText
+        editText = findViewById(R.id.editText);*/
+
+        /*  A activity (tela) inicia capturando o id dos elementos tipo CheckBox*/
+        checkBox_1 = findViewById(R.id.checkbox_1);
+        checkBox_2 = findViewById(R.id.checkbox_2);
     }
 
+    //  Chamado quando o button é clicado
     public void onClick(View view) {
-        /*
-            Ao pressionar o botão aparece a mensagem: "Você pressionou o botão"
+        /*  Ao pressionar o botão aparece a mensagem: "Você pressionou o botão"
         Toast toast = Toast.makeText(this, "Você pressionou o botão", Toast.LENGTH_SHORT);
         toast.show();*/
 
@@ -53,10 +60,45 @@ public class MainActivity extends AppCompatActivity {
         textView.setText("Você pressionou o botão: " + count);*/
 
         /*  Ao pressionar o botão aparece a mensagem: "Você escreveu" acompanhado do que foi digitado
-            pelo usuário */
+            pelo usuário
         String txt = editText.getText().toString();
         Toast toast = Toast.makeText(this, "Você escreveu: " + txt, Toast.LENGTH_SHORT);
-        toast.show();
+        toast.show();*/
 
+        /*  Recebe as instâncias das checkbox*/
+        boolean checked1 = checkBox_1.isChecked();
+        boolean checked2 = checkBox_2.isChecked();
+
+        /*  Verifica o que foi ou não selecionado e exibe na tela através do Toast*/
+        if(checked1 && checked2) {
+            Toast.makeText(this, "Você selecionou as duas opções", Toast.LENGTH_SHORT).show();
+        } else if(checked1) {
+            Toast.makeText(this, "Você selecionou apenas a primeira opção", Toast.LENGTH_SHORT).show();
+        } else if(checked2) {
+            Toast.makeText(this, "Você selecionou apenas a segunda opção", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Você não selecionou nenhuma opção", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //  Chamado quando um dos checkbox é clicado
+    public void onCheckboxClicked(View view) {
+        boolean checked = ((CheckBox) view).isChecked();
+        switch (view.getId()) {
+            case R.id.checkbox_1:
+                if(checked) {
+                    Toast.makeText(this, "Você selecionou a primeira opção", Toast.LENGTH_SHORT);
+                } else {
+                    Toast.makeText(this, "Você cancelou a primeira opção", Toast.LENGTH_SHORT);
+                }
+                break;
+            case R.id.checkbox_2:
+                if(checked) {
+                    Toast.makeText(this, "Você selecionou a segunda opção", Toast.LENGTH_SHORT);
+                } else {
+                    Toast.makeText(this, "Você cancelou a segunda opção", Toast.LENGTH_SHORT);
+                }
+                break;
+        }
     }
 }
