5d2c1930250c8f3d39f8e8b6b10eb567a36f5793
Eduardo Rodrigues Santana <eduardo.r.santana@hotmail.com>
2021-06-23 00:54:57 -0300

    Generating the application's graphical interface

--- aula05/lista_compras/app/src/main/java/br/com/eduardorodrigues/listacompras/MainActivity.java
+++ aula05/lista_compras/app/src/main/java/br/com/eduardorodrigues/listacompras/MainActivity.java
@@ -3,6 +3,7 @@ package br.com.eduardorodrigues.listacompras;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addProduto(View view) {
     }
 }
